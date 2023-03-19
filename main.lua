440fd7af4257c92b9321cdb23e081e37fea0b05c
Bean <finbyg@gmail.com>
2023-03-19 21:48:00 +0000

    1.0

--- main.lua
+++ main.lua
@@ -3,9 +3,6 @@
 local mod = RegisterMod("Commission Template - Items (Passive, Active, Trinket and Card)", 1)
 local game = Game()
 local rng = RNG()
---local RECOMMENDED_SHIFT_IDX = 35
---local seeds = game:GetSeeds()
---local startSeed = seeds:GetStartSeed()
 
 mod.Items = {
     Passive = Isaac.GetItemIdByName("Passive Example"),
@@ -30,7 +27,7 @@ mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseItem)
 function mod:UseCard(card, player)
     if card == mod.Items.Card then
         for i = 1,8 do
-            Isaac.Spawn(5, 10, 0, player.Position, Vector(0,8), nil)
+            Isaac.Spawn(5, 10, 0, player.Position, RandomVector(), nil)
         end
     end
 end
@@ -46,6 +43,14 @@ function mod:CacheEvaluation(player, cacheFlag)
 	if player:HasCollectible(mod.Items.Passive) == true then
 		if cacheFlag == CacheFlag.CACHE_FIREDELAY then
 			player.MaxFireDelay = math.max(1.0, fromTears(toTears(player.MaxFireDelay) + 1.22 * player:GetCollectibleNum(mod.Items.Passive, true)))
+		end
+	end
+	if player:HasTrinket(mod.Items.Trinket) == true then
+		if cacheFlag == CacheFlag.CACHE_SPEED then
+			player.MoveSpeed = player.MoveSpeed - 0.2 * player:GetTrinketMultiplier(mod.Items.Passive)
+		end
+		if cacheFlag == CacheFlag.CACHE_DAMAGE then
+			player.Damage = player.Damage + 2 * player:GetTrinketMultiplier(mod.Items.Passive)
 		end
 	end
 end
